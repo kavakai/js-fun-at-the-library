8d59f36a9a50d5849dbe8f7608e38e78ad20a80c
Kai Kuller <kavakai@gmail.com>
2021-10-13 18:18:23 -0600

    Add variables in warm-up

--- src/warm-up.js
+++ src/warm-up.js
@@ -19,22 +19,22 @@ var pages = 592;
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is
-pages = 592 / 4
- 
+pages = 592 / 4;
+
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie = "";
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
-
+var sequel = `The second ${title}`;
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
-
+var isFavorite = true;
 
 
 
@@ -42,7 +42,7 @@ pages = 592 / 4
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
-
+var isShortStory = pages < 25
 
 
 //------------------------- Conditionals -------------------------//
