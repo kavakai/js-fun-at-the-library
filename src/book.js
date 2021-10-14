5e6816642eca55275b0b00b8c72c17d4d2485b09
Kai Kuller <kavakai@gmail.com>
2021-10-14 17:56:02 -0600

    build creatTitle function, buildMainCharacter function, ghoulCharacter object

--- src/book.js
+++ src/book.js
@@ -1,10 +1,20 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return (`The ${title}`);
 }
+function buildMainCharacter(name, age, pronouns) {
+  ghoulCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return ghoulCharacter
+}
+
+createTitle("Storm's Awakening");
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
