3added616577b18f449fda60990d85f6e1307fdd
Kai Kuller <kavakai@gmail.com>
2021-10-16 17:37:33 -0600

    Finish listTitles function

--- src/shelf.js
+++ src/shelf.js
@@ -9,12 +9,18 @@ function unshelfBook(title, shelf) {
    for (var i = 0; i < shelf.length; i++)
   if (title === shelf[i].title) {
    shelf.splice(i, 1)
- }  console.log(shelf)
+ }
+ // }  console.log(shelf)
  return shelf
 }
 
-function listTitles() {
-  
+function listTitles(shelf) {
+var titleList = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titleList.push(shelf[i].title)
+  }
+console.log(titleList)
+  return titleList.join(", ")
 }
 
 module.exports = {
