37f4fb99ae55e49a3789666bd59d636021b49eba
Kai Kuller <kavakai@gmail.com>
2021-10-17 10:51:57 -0600

    Add checkoutBook function

--- src/Library.js
+++ src/Library.js
@@ -11,10 +11,20 @@ function createLibrary(libraryName) {
 
 function addBook(libraryName, bookTitle) {
  return libraryName.shelves[bookTitle.genre].push(bookTitle);
+}
+
+function checkoutBook(libraryName, bookTitle, genre) {
+  for (var i = 0; i < libraryName.shelves[genre].length; i++) {
+    if (bookTitle === libraryName.shelves[genre][i].title) {
+      libraryName.shelves[genre].splice(i, 1)
+      return `You have now checked out ${bookTitle} from the ${libraryName.name}`
+    }
+  }
 
 }
+
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
